4850d07d0099b7c51324aec779fb192a614342e5
danillob <danillob@gmail.com>
2024-03-04 08:20:46 -0400

    API views definitions

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,5 +1,29 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView, DestroyAPIView
 
-# Create your views here.
-def index(request):
-    return render(request, 'index.html', {})
+from django.contrib.auth.models import User
+from .models import Booking, Menu
+from .serializers import BookingSerializer, MenuItemSerialiazer, UserSerializer
+
+class BookingView(APIView):
+
+    def get(self, request):
+        items = Booking.objects.all()
+        serializer = BookingSerializer(items, many=True)
+        return Response(serializer.data)
+
+class MenuItemView(ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuItemSerialiazer         
+
+class SingleMenuItemView(RetrieveUpdateAPIView, DestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuItemSerialiazer         
+    
+class UserView(APIView):
+
+    def get(self, request):
+        items = User.objects.all()
+        serializer = UserSerializer(items, many=True)
+        return Response(serializer.data)
